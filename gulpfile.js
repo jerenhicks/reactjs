2e2b3fac19cc91ad2dc9ea2a911225686af2bf33
Jeren Hicks <jeren.hicks@gmail.com>
2015-07-09 19:36:00 -0500

    Removed a lot of unused code from the gulp file. Added in production step that minifies both the js and css. This step also removes the debug flags on the React.

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,12 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
-//var streamify = require('gulp-streamify');
+var streamify = require('gulp-streamify');
 var del = require('del');  // Deletes files.
 var runSequence = require('run-sequence');
 var less = require('gulp-less');
 var path = require('path');
+var minifyCSS = require('gulp-minify-css');
 
 var path = {
     HTML: 'src/index.html',
@@ -33,38 +34,9 @@ gulp.task('copyHTML', function(){
 
 });
 
-//gulp.task('replaceHTMLsrc', function(){
-//    gulp.src(path.HTML)
-//        .pipe(htmlreplace({
-//            'js': 'src/' + path.OUT
-//        }))
-//        .pipe(gulp.dest(path.DEST));
-//});
-
 gulp.task('startServer', function() {
     return require('./express.server');
 });
-
-//gulp.task('watch', function() {
-//    gulp.watch(path.HTML, ['replaceHTMLsrc']);
-//
-//    var watcher  = watchify(browserify({
-//        entries: [path.ENTRY_POINT],
-//        transform: [reactify],
-//        debug: true,
-//        cache: {}, packageCache: {}, fullPaths: true
-//    }));
-//
-//    return watcher.on('update', function () {
-//        watcher.bundle()
-//            .pipe(source(path.OUT))
-//            .pipe(gulp.dest(path.DEST_SRC));
-//        console.log('Updated');
-//    })
-//        .bundle()
-//        .pipe(source(path.OUT))
-//        .pipe(gulp.dest(path.DEST_SRC));
-//});
 
 gulp.task('buildReact', function() {
     return browserify({
@@ -77,31 +49,20 @@ gulp.task('buildReact', function() {
         .pipe(gulp.dest('./dist/src/'));
 });
 
+gulp.task('buildReactMin', function() {
+    return browserify({
+        entries: [path.ENTRY_POINT]
+    })  .transform(reactify)
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest('./dist/src/'));
+});
+
 gulp.task('watch', function() {
     return gulp.watch(path.LESS, ['buildLess']);
     return gulp.watch(path.JS, ['buildReact']);
 });
-
-//gulp.task('build', function(){
-//    browserify({
-//        entries: [path.ENTRY_POINT],
-//        transform: [reactify]
-//    })
-//        .bundle()
-//        .pipe(source(path.MINIFIED_OUT))
-//        .pipe(streamify(uglify({file:path.MINIFIED_OUT })))
-//        .pipe(gulp.dest(path.DEST_BUILD));
-//});
-//
-//gulp.task('replaceHTML', function(){
-//    gulp.src(path.HTML)
-//        .pipe(htmlreplace({
-//            'js': 'build/' + path.MINIFIED_OUT
-//        }))
-//        .pipe(gulp.dest(path.DEST));
-//});
-
-//gulp.task('production', ['clean', 'replaceHTML', 'build']);
 
 gulp.task('buildLess', function(){
     return gulp.src('./src/css/styles.less')
@@ -109,6 +70,18 @@ gulp.task('buildLess', function(){
         .pipe(gulp.dest('./dist/css'));
 });
 
+gulp.task('buildLessMin', function(){
+    return gulp.src('./src/css/styles.less')
+        .pipe(less())
+        .pipe(minifyCSS())
+        //possibly rename?
+        .pipe(gulp.dest('./dist/css'));
+});
+
 gulp.task('default', function (cb) {
     runSequence('clean', 'copyHTML', 'buildReact', 'buildLess', 'watch', 'startServer', cb);
+});
+
+gulp.task('production', function (cb) {
+    runSequence('clean', 'copyHTML', 'buildReactMin', 'buildLessMin', cb);
 });
